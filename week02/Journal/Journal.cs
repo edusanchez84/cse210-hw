8a0a28edffd22e382324086d1cf9efc18985fe85
edusanchez84 <san21011@byui.edu>
2025-01-15 01:07:07 +0100

    W02 Journal Design

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Journal
+{
+    public List<Entry> _entryList;
+    
+    public void DisplayAllEntries()
+    {
+
+    }
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadToFile(string file)
+    {
+
+    }
+}
