a850875573b44b5f2648a06d23f54d436f5c5982
edusanchez84 <san21011@byui.edu>
2025-01-20 20:23:57 +0100

    W03 Team Activity - Scripture Memorizer Design

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Reference
+{
+    private string _book;
+    private string _chapter;
+    private string _verse;
+    private string _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+
+    }
+
+    public Reference(string book, int chapter, int startVerse, int enVerse)
+    {
+
+    }
+
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Scripture
+{
+    private Reference reference;
+    private List<Word> _words;
+
+    public Scripture(Reference Reference, string Text)
+    {
+
+    }
+
+    public void HideRandomWords()
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return true;
+    }
+}
