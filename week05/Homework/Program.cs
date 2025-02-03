9dd2c08072f28c7ecda76173791c320efe008ebe
edusanchez84 <san21011@byui.edu>
2025-02-03 23:53:49 +0100

    W05 Learning Activity - Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Ronald Reagan", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment assignment2 = new MathAssignment("Thomas Jefferson", "Percentages", "2", "7");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomework());
+        Console.WriteLine();
+
+        WritingAssignment assignment3 = new WritingAssignment("Gerald Ford", "Classic English", "The prose and past sentences");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+        Console.WriteLine();
     }
 }
